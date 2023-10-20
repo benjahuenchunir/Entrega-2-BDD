b7bc151a1d2443131d2d3fce1bf8a88aa102a135
benjahuenchunir <bahuenchunir@uc.cl>
2023-10-20 16:53:30 -0400

    Deleted mysql connection

--- Website/config/data.php
+++ Website/config/data.php
@@ -2,7 +2,4 @@
   $user = 'grupo131';
   $password = 'grupo131LB';
   $databaseName = 'grupo131e2';
-  $dsn = "mysql:host=localhost;dbname=meta_streaming_service_v2";
-  $username = "root";
-  $pass = "";
 ?>
